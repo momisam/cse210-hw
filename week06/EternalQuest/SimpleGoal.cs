294fdce5d00f35ec04312dd52892d9bbd1678a21
Moses Samuel <mosesfemi10@gmail.com>
2026-02-16 13:07:25 +0100

    ExerciseTracking: Activity.cs

---
+++ week06/EternalQuest/SimpleGoal.cs
@@ -0,0 +1,39 @@
+using System;
+
+public class SimpleGoal : Goal
+{
+    private bool _isComplete;
+
+    public SimpleGoal(string name, string description, int points, bool isComplete = false)
+        : base(name, description, points)
+    {
+        _isComplete = isComplete;
+    }
+
+    public override int RecordEvent()
+    {
+        if (!_isComplete)
+        {
+            _isComplete = true;
+            return _points;
+        }
+
+        return 0;
+    }
+
+    public override bool IsComplete()
+    {
+        return _isComplete;
+    }
+
+    public override string GetDetailsString()
+    {
+        string status = _isComplete ? "[X]" : "[ ]";
+        return $"{status} {_name} ({_description})";
+    }
+
+    public override string GetStringRepresentation()
+    {
+        return $"SimpleGoal|{_name}|{_description}|{_points}|{_isComplete}";
+    }
+}
